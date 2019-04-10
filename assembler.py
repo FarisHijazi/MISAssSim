60eb157edc4fa40862455a72b1b9fb9599ed857b
Faris Hijazi <theefaris@gmail.com>
2019-04-10 16:34:14 +0300

    added reg() to translate function aliases and neumonics

--- assembler.py
+++ assembler.py
@@ -663,6 +663,56 @@ def exitApp():
     sys.exit()
 
 
+def reg(neumonic: str):
+    """
+    given the neumonic (example: $0, $zero, $r1, $v0, etc...)
+    :param neumonic:
+    :return: register number
+    """
+
+    # (key: neumonic, value: registerNumber)
+    registerAliasDict = {
+        '$zero': 0b00000,
+        '$at': 0b00001,
+        '$v0': 0b00010,
+        '$v1': 0b00011,
+        '$a0': 0b00100,
+        '$a1': 0b00101,
+        '$a2': 0b00110,
+        '$a3': 0b00111,
+        '$t0': 0b01000,
+        '$t1': 0b01001,
+        '$t2': 0b01010,
+        '$t3': 0b01011,
+        '$t4': 0b01100,
+        '$t5': 0b01101,
+        '$t6': 0b01110,
+        '$t7': 0b01111,
+        '$s0': 0b10000,
+        '$s1': 0b10001,
+        '$s2': 0b10010,
+        '$s3': 0b10011,
+        '$s4': 0b10100,
+        '$s5': 0b10101,
+        '$s6': 0b11000,
+        '$s7': 0b10111,
+        '$t8': 0b11000,
+        '$t9': 0b11001,
+        '$k0': 0b11010,
+        '$k1': 0b11011,
+        '$gp': 0b11100,
+        '$sp': 0b11101,
+        '$fp': 0b11110,
+        '$ra': 0b11111,
+    }
+
+    if len(neumonic) == 2:
+        return int(neumonic[1:])
+    else:
+        if neumonic in registerAliasDict:
+            return registerAliasDict.get(neumonic, 0)
+
+
 def compileASM():
     global filename
     cpu_out = ""
